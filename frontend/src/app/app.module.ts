c3bf9f4b9447f3b637dcb8f32334bde1e4cda447
VladimirMaestro <102423850+VladimirMaestro@users.noreply.github.com>
2024-07-24 21:04:36 +0300

    add first component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './components/tasks-list/tasks-list.component';
+import { NewTaskFormComponent } from './components/new-task-form/new-task-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    NewTaskFormComponent
   ],
   imports: [
     BrowserModule
---
+++ frontend/src/app/components/new-task-form/new-task-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-new-task-form',
+  templateUrl: 'new-task-form.component.html'
+})
+export class NewTaskFormComponent {
+
+}
