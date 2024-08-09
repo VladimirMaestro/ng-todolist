cfffab4b96553d06e7ec07cdc9e8b7b643c643a1
VladimirMaestro <102423850+VladimirMaestro@users.noreply.github.com>
2024-08-09 23:54:43 +0300

    Components communication

--- frontend/src/app/components/tasks-list/tasks-list.component.ts
+++ frontend/src/app/components/tasks-list/tasks-list.component.ts
@@ -1,9 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Input, Output, ViewEncapsulation } from "@angular/core";
 
 @Component({
   selector: 'app-tasks-list',
-  templateUrl: 'tasks-list.component.html'
+  templateUrl: 'tasks-list.component.html',
+  styleUrls: ['tasks-list.component.scss']
 })
 export class TaskListComponent {
+  @Input('abc') name: string;
+  @Output('abcChange') nameChange: EventEmitter<string> = new EventEmitter();
 
+  ngOnInit(): void {
+    console.log('ngOnInit');
+    console.log(this.name);
+  }
+
+  emitData(): void {
+    this.nameChange.emit('Hello world!');
+  }
 }
